24d13dba3d8c40e2a4d9510220b2969646d1d4ab
David Ochsner <davidochsner93@gmail.com>
2024-12-25 10:42:05 +0100

    Nicer main

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,25 @@
-#![feature(btree_cursors)]
 
 use std::path::PathBuf;
+use clap::Parser;
 
-// mod day01;
-// mod day02;
-// mod day03;
+mod day01;
+mod day02;
+mod day03;
 mod day06;
 
+#[derive(Parser)]
+struct Args {
+    #[arg(short, long, default_value_t = 0)]
+    day: u8,
+}
+
 fn main() {
+    let args = Args::parse();
     let input = PathBuf::from("input");
-    day06::day06(input);
+    match args.day {
+	1 => day01::day01(input),
+	2 => day02::day02(input),
+	3 => day03::day03(input),
+	_ => day06::day06(input),
+    }
 }
