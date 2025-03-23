b42fc553a6107a49befdb8d68f53e91aa540a101
David Ochsner <davidochsner93@gmail.com>
2025-03-23 10:43:24 +0100

    Day08

---
+++ src/day08.rs
@@ -0,0 +1,80 @@
+use std::{
+    cmp,
+    collections::{HashMap, HashSet},
+    fs,
+    path::PathBuf,
+};
+
+use itertools::iproduct;
+
+pub fn day08(mut input_path: PathBuf) {
+    input_path.push("08.txt");
+    let content = fs::read_to_string(input_path).unwrap();
+
+    let (antennas, corner) = read(&content);
+
+    println!("{:?}", count_antinodes(&antennas, ((0, 0), corner), antinodes_part1));
+    println!("{:?}", count_antinodes(&antennas, ((0, 0), corner), antinodes_part2));
+}
+
+// Positive y-direction is up!
+type Position = (i32, i32);
+type ResonancePoints = HashMap<char, Vec<Position>>;
+
+fn read(input: &str) -> (ResonancePoints, (i32, i32)) {
+    let (mut x_max, mut y_max) = (0, 0);
+    let mut antennas = HashMap::new();
+    for (y, line) in input.lines().enumerate() {
+        y_max = y_max.max(y);
+        for (x, c) in line.chars().enumerate() {
+            x_max = x_max.max(x);
+            if c != '.' {
+                antennas
+                    .entry(c)
+                    .or_insert(Vec::new())
+                    .push((x as i32, -(y as i32)))
+            }
+        }
+    }
+    (antennas, (x_max as i32, -(y_max as i32)))
+}
+
+fn count_antinodes(
+    antennas: &ResonancePoints,
+    bounds: (Position, Position),
+    generate_antinodes: fn(&[Position], (Position, Position)) -> Vec<Position>,
+) -> usize {
+    antennas
+        .iter()
+        .flat_map(|(_, positions)| generate_antinodes(&positions, bounds))
+        .collect::<HashSet<_>>()
+        .len()
+}
+
+fn antinodes_part1(antennas: &[Position], bounds: (Position, Position)) -> Vec<Position> {
+    iproduct!(antennas, antennas)
+        .filter(|(p1, p2)| p1 != p2)
+        .map(|((x1, y1), (x2, y2))| (2 * x1 - x2, 2 * y1 - y2))
+        .filter(|p| is_inside(*p, bounds))
+        .collect()
+}
+
+fn antinodes_part2(antennas: &[Position], bounds: (Position, Position)) -> Vec<Position> {
+    iproduct!(antennas, antennas)
+        .filter(|(p1, p2)| p1 != p2)
+        .flat_map(|((x1, y1), (x2, y2))| {
+            (0..)
+                .map(move |i| ((i+1) * x1 - i * x2, (i+ 1) * y1 - i * y2))
+                .take_while(|p| is_inside(*p, bounds))
+        })
+        .collect()
+}
+
+fn is_inside(p: Position, bounds: (Position, Position)) -> bool {
+    // ugh
+    let (x, y) = p;
+    let ((x1, y1), (x2, y2)) = bounds;
+    let [x_min, x_max] = cmp::minmax(x1, x2);
+    let [y_min, y_max] = cmp::minmax(y1, y2);
+    x_min <= x && x <= x_max && y_min <= y && y <= y_max
+}
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
-use std::path::PathBuf;
+#![feature(cmp_minmax)]
+
 use clap::Parser;
+use std::path::PathBuf;
 
 mod day01;
 mod day02;
@@ -8,6 +10,7 @@ mod day04;
 mod day05;
 mod day06;
 mod day07;
+mod day08;
 
 #[derive(Parser)]
 struct Args {
@@ -19,12 +22,13 @@ fn main() {
     let args = Args::parse();
     let input = PathBuf::from("input");
     match args.day {
-	1 => day01::day01(input),
-	2 => day02::day02(input),
-	3 => day03::day03(input),
-	4 => day04::day04(input),
-	5 => day05::day05(input),
-	7 => day07::day07(input),
-	_ => day06::day06(input),
+        1 => day01::day01(input),
+        2 => day02::day02(input),
+        3 => day03::day03(input),
+        4 => day04::day04(input),
+        5 => day05::day05(input),
+        6 => day06::day06(input),
+        7 => day07::day07(input),
+        _ => day08::day08(input),
     }
 }
