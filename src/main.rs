a2d1f9c0ff39b74978ac6e19c6668b1b05b19222
David Ochsner <davidochsner93@gmail.com>
2025-07-06 10:27:17 +0200

    Only run once when testing

--- src/main.rs
+++ src/main.rs
@@ -21,6 +21,7 @@ mod day10;
 mod day11;
 mod day12;
 mod day13;
+mod day14;
 
 #[derive(Parser)]
 struct Args {
@@ -34,12 +35,11 @@ struct Args {
     test: bool,
 }
 
-fn timed<F, R>(f: F, path: &str, label: &str)
+fn timed<F, R>(f: F, path: &str, label: &str, n: usize)
 where
     F: Fn(&str) -> R,
     R: Debug,
 {
-    let n = 5;
     let input = fs::read_to_string(path).unwrap();
     let start = Instant::now();
     for _ in 1..n {
@@ -48,7 +48,7 @@ where
     let res = f(&input);
     let end = Instant::now();
     println!("{}: {:?}", label, res);
-    println!("Average: {:.2?}", (end - start) / 5);
+    println!("Average: {:.2?}", (end - start) / n as u32);
 }
 
 fn call_timed(fn_and_label: (Solution, &str), base: &str, test: bool) {
@@ -61,6 +61,7 @@ fn call_timed(fn_and_label: (Solution, &str), base: &str, test: bool) {
             if test { "_test" } else { "" }
         ),
         fn_and_label.1,
+        if test { 1 } else { 5 },
     );
 }
 
@@ -83,6 +84,7 @@ fn main() {
         (day11::solve, "day11"),
         (day12::solve, "day12"),
         (day13::solve, "day13"),
+        (day14::solve, "day14"),
     ];
 
     // underflow is fine
